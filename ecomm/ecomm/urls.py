91e83f25c873d7ade8a3bd4ed54f4b8f92301b3b
Chalmers Campbell <123522718+Chalmers4@users.noreply.github.com>
2023-04-24 21:55:58 -0500

    Add files via upload

--- ecomm/ecomm/urls.py
+++ ecomm/ecomm/urls.py
@@ -1,23 +1,37 @@
-"""
-URL configuration for ecomm project.
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from django.contrib.auth import views as auth_view
+from . import views
+from .forms import LoginForm, MyPasswordResetForm
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("app.urls")),
-]
+    path('', views.home),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
+    path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
+    path("productdetail/<int:pk>", views.ProductDetail.as_view(), name="productdetail"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("address/", views.address, name="address"),
+    path('updateAddress/<int:pk>', views.updateAddress.as_view(), name='updateAddress'),
+    
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
+    path('checkout/', views.checkout.as_view(), name='checkout'),
+    path('paymentdone/', views.payment_done, name='paymentdone'),
+    path('orders/', views.home, name='orders'),
+
+    path('pluscart/', views.plus_cart),
+    path('minuscart/', views.minus_cart),
+    path('removecart/', views.remove_cart),
+
+
+    #login authentication
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'),
+    
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
